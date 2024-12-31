7656cd9cab9d0b1ead004aa61c65dc341aafd0ed
Darkmeedo <113542244+Darkmeedo@users.noreply.github.com>
2024-12-31 05:27:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./assets/components/Navbar";
-import "./App.css";
+import "./App.sass";
 import Banner from "./assets/components/Banner";
 import FlatGrid from "./assets/components/FlatGrid.jsx";
 import Main from "./assets/components/Main.jsx";
